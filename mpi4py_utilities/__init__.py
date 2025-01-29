e6f509be378cb8cf4e8eb2f5e22808a5f0a36bb0
Leon Foks <leonfoks@gmail.com>
2025-01-29 13:19:11 -0600

    Added the mpi enabled pseudo rng

--- mpi4py_utilities/__init__.py
+++ mpi4py_utilities/__init__.py
@@ -4,6 +4,7 @@ from os import getpid
 import pickle
 import numpy as np
 from numpy.random import Generator, PCG64DXSM
+from .src.common import prng
 from .src.common import print, load_balance, mpiu_time
 from .src.Bcast import Bcast
 from .src.Send import Send
@@ -201,49 +202,3 @@ def hello_world(world):
 #         this = np.reshape(this_unpk, [chunks[world.rank], s])
 
 #         return this.T if axis == 1 else this
-
-# def get_prng(generator=PCG64DXSM, seed=None, jump=None, world=None):
-#     """Generate an independent prng.
-
-#     Returns
-#     -------
-#     seed : int or file, optional
-#         The seed of the bit generator.
-#     jump : int, optional
-#         Jump the bit generator by this amount
-#     world : mpi4py.MPI.COMM_WORLD, optional
-#         MPI communicator, will jump each bit generator by world.rank
-
-#     """
-#     # Default to single core, else grab the mpi rank.
-#     rank = 0
-#     if world is not None:
-#         rank = world.rank
-
-#     if rank == 0:
-#         if seed is not None: # Seed is a file.
-#             if isinstance(seed, str):
-#                 with open(seed, 'rb') as f:
-#                     seed = pickle.load(f)
-#             assert isinstance(seed, int), TypeError("Seed {} must have type python int (not numpy)".format(seed))
-
-#         else: # No seed, generate one
-#             bit_generator = generator()
-#             seed = bit_generator.seed_seq.entropy
-#             with open('seed.pkl', 'wb') as f:
-#                 pickle.dump(seed, f)
-#             print('Seed: {}'.format(seed), flush=True)
-
-#     if world is not None:
-#         # Broadcast the seed to all ranks.
-#         seed = world.bcast(seed, root=0)
-
-#     bit_generator = generator(seed = seed)
-
-#     if world is not None:
-#         jump = world.rank
-
-#     if jump is not None:
-#         bit_generator = bit_generator.jumped(jump)
-
-#     return Generator(bit_generator)
